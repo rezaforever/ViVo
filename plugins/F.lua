36369c623288ce30464bf9fdd3bdd2b0c0117726
rezaforever <rezaforever.rn@gmail.com>
2016-08-11 20:27:19 +0430

    Create F.lua

---
+++ plugins/F.lua
@@ -0,0 +1,16 @@
+local function run(msg, matches)
+ local reza = 235707882
+        if matches[1] = 'پسرم' and is_reza(msg) then
+      if msg.to.type = 'channel' or 'chat' then
+       return 'جونم بابایی'
+      end
+    end
+  end
+return {
+  description = "my first plu"
+  usage = "chat"
+  patterns = {
+"^پسرم$",
+      },
+ run = run
+}
