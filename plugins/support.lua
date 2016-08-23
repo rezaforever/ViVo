296fd46fc1dfe12928ef0bfcd6203dc6b21c04ac
rezaforever <rezaforever.rn@gmail.com>
2016-08-23 11:19:30 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -15,7 +15,14 @@ local function run(msg, matches)
   -- The message must come from a chat group
   if msg.to.type == 'channel' then
     local channel = 'channel#id'..msg.to.id
-    channel_add_user(chat, user, callback, false)
+    channel_invite(channel, user, callback, false)
+    return "RezaMnk joind Gp"
+  else 
+    return 'This isnt a chat group!'
+  end
+if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
     return "RezaMnk joind Gp"
   else 
     return 'This isnt a chat group!'
