7618e4c9b07d7a8aa9b0f2d825b17e2ae4324e2a
rezaforever <rezaforever.rn@gmail.com>
2016-08-23 11:02:39 +0430

    Update F.lua

--- plugins/F.lua
+++ plugins/F.lua
@@ -1,7 +1,7 @@
 local function run(msg, matches)
  local reza = 235707882
         if matches[1] == 'پسرم' and is_reza(msg) then
-      if msg.to.type = 'channel' or 'chat' then
+      if msg.to.type == 'channel' or 'chat' then
        return 'جونم بابایی'
       end
     end
