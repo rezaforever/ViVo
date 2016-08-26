1be8ec595f0425ef592a11a675f251eb06a85686
rezaforever <rezaforever.rn@gmail.com>
2016-08-26 23:33:47 +0430

    Update delmsg.lua

--- plugins/delmsg.lua
+++ plugins/delmsg.lua
@@ -1,37 +1,31 @@
 local function history(extra, suc, result)
-  for i=10000000000000, #result do
+  for i=1, #result do
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" Recently messages removed', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" Recentry messages has been removed !', ok_cb, false)
   else
-    send_msg(extra.chatid, 'ℹ️Mention messages removed', ok_cb, false)
+    send_msg(extra.chatid, 'Mentioned messages has been removed !', ok_cb, false)
   end
 end
 local function run(msg, matches)
-  if matches[1] == 'delmsg' and is_owner(msg) then
+  if matches[1] == 'del' and is_owner(msg) then
     if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 1000000000 or tonumber(matches[2]) < 1 then
-        return "Select more than 1 and less than 1000000000"
+      if tonumber(matches[2]) > 10000000000 or tonumber(matches[2]) < 1 then
+        return "Please choose more than 1"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "ℹ️Just supergroup"
+      return "Just sopergroup"
     end
   else
-    return "ℹ️This command need privilege"
+    return "You can not clean msgs"
   end
-     local x = "1000000000"
-    if matches[1] == "delmsg" and matches[2] == "x" and is_owner(msg) then
-   if msg.to.type == 'channel' then
-  return "["..msg.to.id.."] Messages has been removed succesfully !"
- end
 end
-end 
+
 return {
     patterns = {
-        '^[!/#](delmsg) (%d*)$',
-        '^[!/#](delmsg) x$'
+        '^[!/#](del) (%d*)$'
     },
     run = run
 }
