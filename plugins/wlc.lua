ec537b8925cb0866aaed7aec4363eb35ee68d25e
rezaforever <rezaforever.rn@gmail.com>
2016-08-27 00:01:07 +0430

    Update wlc.lua

--- plugins/wlc.lua
+++ plugins/wlc.lua
@@ -8,7 +8,7 @@ local group_welcome = redis:hget(hash,'welcome')
 if matches[1] == 'delwlc' and not matches[2] and is_owner(msg) then 
     
    redis:hdel(hash,'welcome')
-        return 'Group wlc deleted'
+        return 'Wlc msg removed'
 end
 
 local url , res = http.request('http://api.gpmod.ir/time/')
@@ -17,22 +17,18 @@ local jdat = json:decode(url)
 
 if matches[1] == 'setwlc' and is_owner(msg) then
 redis:hset(hash,'welcome',matches[2])
-        return 'Gp wlc set to : \n'..matches[2]
+        return 'Wlc message has been set to : \n'..matches[2]
 end
 
-if matches[1] == 'chat_add_user_link' or 'chat_add_user' or 'channel_invite' and msg.service then
-group_welcome = string.gsub(group_welcome, '{gpname}', msg.to.title)
- group_welcome = string.gsub(group_welcome, '{firstname}', ""..(msg.action.user.first_name or '').."")
-  group_welcome = string.gsub(group_welcome, '{lastname}', ""..(msg.action.user.last_name or '').."")
-  group_welcome = string.gsub(group_welcome, '{username}', "@"..(msg.action.user.username or '').."")
-  group_welcome = string.gsub(group_welcome, '{fatime}', ""..(jdat.FAtime).."")
-  group_welcome = string.gsub(group_welcome, '{entime}', ""..(jdat.ENtime).."")
-  group_welcome = string.gsub(group_welcome, '{fadate}', ""..(jdat.FAdate).."")
-  group_welcome = string.gsub(group_welcome, '{endate}', ""..(jdat.ENdate).."")
-  group_welcome = string.gsub(group_welcome, '{rules}', ""..(rules or '').."")
-  group_welcome = string.gsub(group_welcome, '{about}', ""..(about or '').."")
+if matches[1] == 'channel_invite' and msg.service then
+group_welcome = string.gsub(group_welcome, '{gp}', msg.to.title)
+ group_welcome = string.gsub(group_welcome, '{fa}', ""..(jdat.FAtime).."")
+  group_welcome = string.gsub(group_welcome, '{en}', ""..(jdat.ENtime).."")
+  group_welcome = string.gsub(group_welcome, '{ft}', ""..(jdat.FAdate).."")
+  group_welcome = string.gsub(group_welcome, '{et}', ""..(jdat.ENdate).."")
+  group_welcome = string.gsub(group_welcome, '{r}', ""..(rules or '').."")
+  group_welcome = string.gsub(group_welcome, '{a}', ""..(about or '').."")
   group_welcome = string.gsub(group_welcome, '{id}', ""..(msg.from.id).."") 
-
 
  end
 return group_welcome
@@ -41,9 +37,7 @@ return {
   patterns = {
   "^[!#/](setwlc) +(.*)$",
   "^[!#/](delwlc)$",
-  "^!!tgservice (chat_add_user)$",
-  "^!!tgservice (channel_invite)$",
-  "^!!tgservice (chat_add_user_link)$",  
+  "^!!tgservice (channel_invite)$",     
   },
   run = run
 }
