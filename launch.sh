eb84590ed315296263859287ac8f35b2e5df78c4
rezaforever <rezaforever.rn@gmail.com>
2016-08-28 14:53:06 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
+--by @Hextor_ch
+
 #!/usr/bin/env bash
-# fix crash server by mustafa ip( @HackeD_o )
+
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
 
@@ -58,21 +60,6 @@ install_rocks() {
     then echo "Error. Exiting."; exit $RET;
   fi
 
-  ./.luarocks/bin/luarocks install luafilesystem
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install lub
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-
-  ./.luarocks/bin/luarocks install luaexpat
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi  
-  
   ./.luarocks/bin/luarocks install xml
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
@@ -82,23 +69,8 @@ install_rocks() {
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
-  
+
   ./.luarocks/bin/luarocks install serpent
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-  
-  ./.luarocks/bin/luarocks install lunitx
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-  
-  ./.luarocks/bin/luarocks install set
-  RET=$?; if [ $RET -ne 0 ];
-    then echo "Error. Exiting."; exit $RET;
-  fi
-  
-  ./.luarocks/bin/luarocks install htmlparser
   RET=$?; if [ $RET -ne 0 ];
     then echo "Error. Exiting."; exit $RET;
   fi
@@ -107,15 +79,16 @@ install_rocks() {
 install() {
   git pull
   git submodule update --init --recursive
-  cd tg && ./configure && make
-  
-  RET=$?; if [ $RET -ne 0 ]; then
-    echo "Trying without Python...";
-    ./configure --disable-python && make
-    RET=$?
-  fi
-  
+  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  RET=$?;
+
+  cd tg
   if [ $RET -ne 0 ]; then
+    autoconf -i
+  fi
+  ./configure && make
+
+  RET=$?; if [ $RET -ne 0 ]; then
     echo "Error. Exiting."; exit $RET;
   fi
   cd ..
@@ -133,20 +106,17 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  
-   chmod 777 launch.sh
-   
+
   if [ ! -f ./tg/bin/telegram-cli ]; then
     echo "tg binary not found"
     echo "Run $0 install"
     exit 1
   fi
- 
- rm -r ~/.telegram-cli/state
- 
- echo "Anti CRASH server is : ON"
- 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/OmeGa.lua -l 1 -E
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/OmeGa.lua -l 1 -E $@
+   sleep 3
+  done
 fi
 
-#FIX CRASH SERVER BY MUSTAFA IP
+--by @Hextor_ch
