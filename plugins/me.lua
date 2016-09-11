c2eca542f85e6253ea33cd7737d80c98c4559f1c
rezaforever <rezaforever.rn@gmail.com>
2016-09-11 14:02:43 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -4,27 +4,27 @@ do
 local function alirezapt(msg ,matches)
 local reza = 235707882
         if matches[1] == 'me' and is_sudo(msg) then
-local url = "http://www.iloveheartstudio.com/-/p.php?t=Sudo%20%EE%BB%AE%EE%BB%AE&bc=FFFFFF&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+local url = "www.keepcalmstudio.com/-/p.php?t=%3E%EE%BC%84%0D%0A%3E%3E%3EKEEP%0D%0A%3E%3ECALM%0D%0A%3E%3Ebecause%0D%0A%3EYou%27re%0D%0A<3Sudo<3&bc=FFFFFF&tc=FF0000&cc=FF0000&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 local file = download_to_file(url,'alirezapt.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
         
         elseif matches[1] == 'me' and is_owner(msg) then
-        local url = "http://www.iloveheartstudio.com/-/p.php?t=Owner%20%EE%BB%AE%EE%BB%AE&bc=FFFFFF&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+        local url = "www.keepcalmstudio.com/-/p.php?t=%3E%EE%BC%84%0D%0A%3E%3E%3EKEEP%0D%0A%3E%3ECALM%0D%0A%3E%3Ebecause%0D%0A%3EYou%27re%0D%0AOwner&bc=FFFFFF&tc=FF0000&cc=FF0000&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 local file = download_to_file(url,'alirezapt.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
         
         elseif matches[1] == 'me' and is_momod(msg) then
-        local url = "http://www.iloveheartstudio.com/-/p.php?t=Gp%20admin%20%EE%BB%BC&bc=FFFFFF&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+        local url = "www.keepcalmstudio.com/-/p.php?t=%3E%EE%BC%84%0D%0A%3E%3E%3EKEEP%0D%0A%3E%3ECALM%0D%0A%3E%3Ebecause%0D%0A%3EYou%27re%0D%0AGp<3Admin&bc=FFFFFF&tc=FF0000&cc=FF0000&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 local file = download_to_file(url,'alirezapt.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
         
         elseif matches[1] == 'me' and not is_momod(msg) then
-        local url = "http://www.iloveheartstudio.com/-/p.php?t=Member&bc=FFFFFF&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+        local url = "www.keepcalmstudio.com/-/p.php?t=%3E%EE%BC%84%0D%0A%3E%3E%3EKEEP%0D%0A%3E%3ECALM%0D%0A%3E%3Ebecause%0D%0A%3EYou%27re%0D%0Amember&bc=FFFFFF&tc=FF0000&cc=FF0000&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 local file = download_to_file(url,'alirezapt.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
         
-        elseif matches[1] == 'me' and is_reza(msg) then
-        local url = "http://www.iloveheartstudio.com/-/p.php?t=%EE%BB%AA%0D%0ABabajoon&bc=FFFFFF&tc=000000&hc=FF0000&f=c&uc=true&ts=true&ff=PNG&w=500&ps=sq"
+        elseif matches[1] == 'me' and msg.action.user.id == tonumber(reza) then
+        local url = "www.keepcalmstudio.com/-/p.php?t=%3E%EE%BC%84%0D%0A%3E%3E%3EKEEP%0D%0A%3E%3ECALM%0D%0A%3E%3Ebecause%0D%0A%3EYou%27re%0D%0AMy<3Love&bc=FFFFFF&tc=FF0000&cc=FF0000&uc=true&ts=true&ff=PNG&w=500&ps=sq"
 local file = download_to_file(url,'alirezapt.webp')
 send_document(get_receiver(msg) , file, ok_cb, false)
         end
