9cc4d11cb00b777d67905ab12ad4e48c62e60f8c
rezaforever <rezaforever.rn@gmail.com>
2016-09-18 11:02:31 +0430

    Update baba.lua

--- plugins/baba.lua
+++ plugins/baba.lua
@@ -1,32 +1,34 @@
-local function omega(msg,matches)
+local function run(msg,matches)
 
-local mamani = 86957134
-local kiana = 191138680
+
 local khale = {'بله خاله کیانا','بله خاله جون'}
 local maman = {'جونم مامان جون ؟','آخ جون مامانی کار داره','جونم مامانی'}
 local baba = {'جونم بابا جونــــــــــز','بابایی کارم داره همه سکـــــوت 😎','وایی بابا جونم اومده 😜'}
-if matches[1] == "پسرم" and msg.action.user.id == tonumber(mamani) then
-return "<b>".. maman[math.random(#maman)].."</b>"
+local text = maman[math.random(#maman)]
+local text2 = baba[math.random(#baba)]
+local text3 = khale[math.random(#khale)]
+if matches[1] == "پسرم" and msg.action.user.id == 86957134 then
+return "<b>"..text.."</b>"
 end
 if matches[1] == "پسرم" and is_sudo(msg) then
-return "<b>".. baba[math.random(#baba)].."</b>"
+return "<b>"..text2.."</b>"
 end
-if matches[1] == "امگا" or matches[1] == "omega" and msg.action.user.id == tonumber(kiana) then
-return "<b>".. khale[math.random(#khale)].."</b>"
+if matches[1] == "امگا" or matches[1] == "omega" and msg.action.user.id == 191138680 then
+return "<b>"..text3.."</b>"
 end
-if matches[1] == "پسرم" and not is_sudo(msg) or not msg.action.user.id == tonumber(mamani) then
-return "<b>من بابا و مامان خودمو دارم 🙄</b>"
+if matches[1] == "پسرم" and not is_sudo(msg) or not msg.action.user.id == 86957134 then
+return "<i>من بابا و مامان خودمو دارم 🙄</i>"
+end
 end
 
 return {
   patterns = {
- "^پسرم$",
-"^omega$",
-"^امگا$",
+ "^(پسرم)$",
+"^(omega)$",
+"^(امگا)$",
 },
-  run = omega
+  run = run
 }
-end
 
 --copy without source : 🚫
 --@OmeGaTeam
